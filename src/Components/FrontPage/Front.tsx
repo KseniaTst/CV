9b0adbf8a7b1318a189fed02a8f43518b1ba6ad4
KseniaTst <98537255+KseniaTst@users.noreply.github.com>
2022-05-17 23:42:52 +0300

    Initialize project using Create React App

--- src/Components/FrontPage/Front.tsx
+++ src/Components/FrontPage/Front.tsx
@@ -9,7 +9,7 @@ export const Front = () => {
                     Меня зовут Ксения<br/>
                     Я front-end разработчик
                 </div>
-                <div><img className={s.img} src={'https://psv4.userapi.com/c236331/u182082274/docs/d15/4247eef6c1da/IMG_20220510_182738_102.jpg?extra=Dazc2X4CrVcLufoJ5-FbSPaQrDyp5qgE7X-9E8chDxDcBFhKvEyz_EuFcgkasnKJYnV7O2rZfFmY-51x-LvsfPs5Ih5_XFCoyMnuGJdAdvaAZwpUaavLEGHSpJV7CdLzcp8babu2kQi-U_QrdLfMfK-A'}/></div>
+                <div><img className={s.img} src={''}/></div>
             </Paper>
         </div>
     )
