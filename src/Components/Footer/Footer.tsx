08e52315dd21b61fc4964a9c27d5857cbec87ab6
Ksenia Tst <kseniatsytrikova@gmail.com>
2022-06-17 21:07:17 +0300

    Initialize project using Create React App

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,12 +1,16 @@
 import s from "./Footer.module.css";
+import { BottomNavigation } from '@mui/material';
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 
-export const Footer=()=>{
-    return (
-        <div className={s.main}>
-                <footer>
-                    <p>2022 All rights reserved</p>
-                </footer>
-        </div>
-    )
-}
+ export const Footer=()=>{
+   return (
+         <div className={s.main}>
+                 <footer>
+                     <p>2022 All rights reserved</p>
+                 </footer>
+         </div>
+     )
+ }
