278ae8494712045560a898ad1ab860cb41937de8
Ksenia Tst <kseniatsytrikova@gmail.com>
2022-07-24 14:54:45 +0300

    change scss files adding mixins

--- src/Components/Contacts/Contacts.tsx
+++ src/Components/Contacts/Contacts.tsx
@@ -1,27 +1,40 @@
 import s from './Contacts.module.scss'
+
 import {BigHeader} from "../TextHeaders/BigHeader";
 import {LittleHeader} from "../TextHeaders/LittleHeader";
 import {CustomButton} from "../Button";
+import { motion } from 'framer-motion';
 
 export const Contacts=()=>{
+    const formAnimation ={
+        hidden:{
+            opacity: 0
+        },
+        visible: {
+            opacity: 1,
+            'transition-timing-function': 'ease',
+            'transition-duration': '2s'
+        }
+    }
+
     return (
-        <div className={s.main}>
+        <motion.div className={s.main} initial='hidden' whileInView='visible' viewport={{amount:0.3, once:true}}>
             <div className={s.container}>
                 <LittleHeader text={'Get in touch'}/>
                 <BigHeader text={'Contact me'}/>
-                <div className={s.formContainer } >
-                    <form className={s.form} >
-                        <div className={s.inputContainer}>
+                <div className={s.formContainer}  >
+                    <motion.form className={s.form} variants={formAnimation} >
+                        <div className={s.inputContainer} >
                             <div className={s.formInputContainer}><input placeholder={'Name'} className={s.formInput}/></div>
                             <div className={s.formInputContainer}> <input placeholder={'Email'} className={s.formInput}/></div>
                         </div>
                         <textarea placeholder={'Message'} />
-                    </form>
+                    </motion.form>
                 </div>
                 <div >
                     <CustomButton text={'Send message'}/>
                 </div>
             </div>
-        </div>
+        </motion.div>
     )
 }
--- src/Components/TextHeaders/BigHeader.tsx
+++ src/Components/TextHeaders/BigHeader.tsx
@@ -1,10 +1,13 @@
 import s from './Header.module.scss'
+const Fade = require('react-reveal/Fade')
 
 type PropsType={
     text:string
 }
 export const BigHeader=(props:PropsType)=>{
     return(
+        <Fade bottom cascade>
         <span className={s.span}>{props.text}</span>
+        </Fade>
     )
 }
