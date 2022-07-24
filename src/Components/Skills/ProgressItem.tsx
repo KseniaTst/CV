ae0019f83e5b12feebd2a63f44a1912e5a6fe03e
Ksenia Tst <kseniatsytrikova@gmail.com>
2022-07-24 17:42:24 +0300

    progress item and skills animation

--- src/Components/Skills/ProgressItem.tsx
+++ src/Components/Skills/ProgressItem.tsx
@@ -1,17 +1,36 @@
-import s from './ProgressItem.module.scss'
+import s from './ProgressItem.module.scss';
+import { motion } from "framer-motion"
 
 type PropsType = {
     value: string
     skillName: string
 }
 
+
+
 export const ProgressItem=(props:PropsType)=>{
+
+const progressAnimation ={
+    hidden:{
+        width: 0
+    },
+    visible: {
+        width: props.value,
+        'transition-timing-function': 'ease',
+        'transition-duration': '2s'
+    }
+}
+
     return(
-        <div className={s.skillItem}>
+        <motion.div className={s.skillItem}  initial='hidden' whileInView='visible' viewport={{amount:0.1, once:true}}>
             <h6>{props.skillName}</h6>
             <div className={s.skillProgress}>
-                <div title={props.value}  className={s.progress}/>
+                <motion.div
+                    title={props.value}
+                    className={s.progress}
+                    variants={progressAnimation}
+                     />
             </div>
-        </div>
+        </motion.div>
     )
 }
--- src/Components/Skills/Skills.tsx
+++ src/Components/Skills/Skills.tsx
@@ -1,9 +1,12 @@
 import s from './Skills.module.scss'
 import {ProgressItem} from "./ProgressItem";
+const Fade = require('react-reveal/Fade')
+
 
 export const Skills = () => {
     return (
         <div className={s.main}>
+
             <div className={s.container}>
                 <div className={s.textAbout}>
                     <div className={s.subTitle}>
@@ -13,17 +16,20 @@ export const Skills = () => {
                         <h6> ABOUT ME</h6>
 
                     </div>
+                    <Fade bottom>
                     <p className={s.txtBig}>Some text with big letters</p>
                     <p className={s.txtSmall}>Some text about me with little letters</p>
                     <p>Download CV</p>
+                    </Fade>
                 </div>
                 <div className={s.skills}>
                     <ProgressItem value={'80%'} skillName={'REACT'}/>
                     <ProgressItem value={'90%'} skillName={'REDUX'} />
-                    <ProgressItem value={'70%'} skillName={'CSS'}/>
-                    <ProgressItem value={'85%'} skillName={'ENGLISH'}/>
+                    <ProgressItem value={'70%'} skillName={'SCSS'} />
+                    <ProgressItem value={'85%'} skillName={'ENGLISH (upper intermediate)'}/>
                 </div>
             </div>
+
         </div>
     )
 }
