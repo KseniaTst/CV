2fa70493c597bee709fb7cef19d829ed2e8c3654
Ksenia Tst <kseniatsytrikova@gmail.com>
2022-07-26 15:34:28 +0300

    some settings

--- src/Components/TextHeaders/LittleHeader.tsx
+++ src/Components/TextHeaders/LittleHeader.tsx
@@ -1,10 +1,13 @@
 import s from './Header.module.scss'
+const Reveal = require('react-reveal/Reveal')
 
 type PropsType={
     text:string
 }
 export const LittleHeader=(props:PropsType)=>{
     return(
+        <Reveal >
         <h6 className={s.little}>{props.text}</h6>
+        </Reveal>
     )
 }
--- src/Components/Works/OneWork.tsx
+++ src/Components/Works/OneWork.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import s from './Works.module.scss'
 
 
@@ -5,17 +6,23 @@ type PropsType={
     icon:JSX.Element
     projectName:string
     description:string
+    title:string
+    address:string
 }
 
-export const OneWork=(props:PropsType)=>{
-    return(
-        <div className={s.oneWork} >
-            <div className={s.watch}>
-                {props.icon}
-            </div>
-            <div className={s.description}>
+export const OneWork = (props: PropsType) => {
+
+    return (
+        <div className={s.oneWork}>
+            <motion.div className={s.watch} id={props.title}  >
+                <motion.img  src={props.address} whileHover={{scale:1.1, opacity:0.4}}/>
+                <motion.span className={s.span} >click to watch</motion.span>
+                {/*{props.icon}*/}
+            </motion.div>
+            <div className={s.projectName}>
                 <p>{props.projectName}</p>
-                <div>{props.description}</div>
+                <div  >{props.description}</div>
+
             </div>
         </div>
     )
