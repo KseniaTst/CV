671250771b587281d749415e174f462ff30d56a2
Ksenia Tst <kseniatsytrikova@gmail.com>
2022-08-08 18:37:04 +0300

    some settings

--- src/Components/Works/Works.tsx
+++ src/Components/Works/Works.tsx
@@ -3,7 +3,9 @@ import {OneWork} from "./OneWork";
 import {socialNet, todolist} from "../../assets/icons/icons";
 import {BigHeader} from "../TextHeaders/BigHeader";
 import {LittleHeader} from "../TextHeaders/LittleHeader";
+import TodoPh from '../../assets/images/todolist.jpg'
 const Slide = require('react-reveal/Slide')
+
 
 
 
@@ -19,11 +21,20 @@ export const Works = () => {
                 <Slide bottom>
                     <OneWork icon={socialNet} projectName={'Social network demo'} title={'socialNet'}
                              imgAddress={'https://www.pixelstalk.net/wp-content/uploads/2015/01/Colorful-3D-Social-Media-Wallpaper-Widescreen-HD-620x413.jpg'}
-                             description={'vTempore corrupti temporibus fuga earum asperiores fugit laudantium.'}
+                             description={'Demo version of social network. Was developed using Typescript, React, Redux,' +
+                                 'Redux Thunk, Router-dom, Axios, Mui for some' +
+                                 ' components, SCSS, Formik, Bug fixing, deployin. '}
                     address={'https://kseniatst.github.io/Social-network/#/profile'}/>
                     <OneWork icon={todolist} projectName={'Todolist demo '} title={'todo'}
-                             imgAddress={'https://scontent.fgme1-1.fna.fbcdn.net/v/t39.30808-6/228029020_4455379694506012_688416722599575150_n.jpg?stp=dst-jpg_p843x403&_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=2LvynofDa8sAX8YzRVE&_nc_ht=scontent.fgme1-1.fna&oh=00_AT-4MBLDFW27D2QamAPX-siFbLY9c_geZzQZnWwlzwn5Ig&oe=62E15D03'}
-                             description={'Tempore corrupti temporibus fuga earum asperiores fugit laudantium.'}
+                             imgAddress={TodoPh}
+                             description={'Todolist demo version with using ' +
+                                 'React, Redux, Redux-Toolkit, React router-\n' +
+                                 'dom v6, Jest(Unit-Tests), Storybook, Axios,\n' +
+                                 '\n' +
+                                 'Formik, Material UI.\n' +
+                                 'Todolist app created with react hooks, routing\n' +
+                                 'and redirects, material UI, REST api\n' +
+                                 'request/response flow, unit-tests and etc.'}
                     address={'https://KseniaTst.github.io/Todolist'}/>
                 </Slide>
             </div>
