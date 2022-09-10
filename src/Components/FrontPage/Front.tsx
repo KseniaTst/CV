a9c2b2fd10af17c2cb7aa87db50be9eaf9294cd7
Ksenia Tst <kseniatsytrikova@gmail.com>
2022-09-11 02:43:34 +0300

    change front page

--- src/Components/FrontPage/Front.tsx
+++ src/Components/FrontPage/Front.tsx
@@ -2,30 +2,45 @@ import s from './Front.module.scss'
 import TelegramIcon from '@mui/icons-material/Telegram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import MyPh from '../../assets/images/1659277940157_20220731_171542_mr1659277939593_mh1659277980413.jpg'
+import MyPh from '../../assets/images/myPhoto.jpg'
+import {MouseParallaxContainer, MouseParallaxChild} from "react-parallax-mouse";
 
 export const Front = () => {
+
     return (
         <div className={s.main} id={'front'}>
-            <img src={MyPh}/>
-            <nav className={s.nav}>
-                <a href={'#front'}>Home</a>
-                <a href={'#skills'}>Skills</a>
-                <a href={'#works'} >My works</a>
-                <a href={'#contacts'} >Contacts</a>
-            </nav>
-            <div className={s.container} >
-                <div className={s.row}>
-                    <h6>HELLO, I'M</h6>
-                    <h1>Ksenia Tsytrikova</h1>
-                    <h4>FRONTEND DEVELOPER</h4>
-                    <div className={s.socialBox}>
-                       <a href={'https://t.me/KseniaTst'}> <TelegramIcon fontSize={'large'} style={{color: 'white', paddingRight: '15px'}}/></a>
-                       <a href={'https://github.com/KseniaTst'}> <GitHubIcon fontSize={'large'} style={{color: 'white', paddingRight: '15px'}}/></a>
-                        <a href={'https://www.linkedin.com/in/ksenia-tsytrikova-993b66240/'}><LinkedInIcon fontSize={'large'} style={{color: 'white', paddingRight: '15px', height:'35px'}}/></a>
+            <MouseParallaxContainer className={s.main}>
+                <MouseParallaxChild factorX={0.03} factorY={0.05} className={s.imgContainer}>
+                    <img src={MyPh}/>
+                </MouseParallaxChild>
+                <MouseParallaxChild factorX={0.03} factorY={0.05} className={s.imgFrame}/>
+
+                <nav className={s.nav}>
+                    <a href={'#front'}>Home</a>
+                    <a href={'#skills'}>Skills</a>
+                    <a href={'#works'}>My works</a>
+                    <a href={'#contacts'}>Contacts</a>
+                </nav>
+                <div className={s.container}>
+                    <div className={s.row}>
+                        <h6>HELLO, I'M</h6>
+                        <h1>Ksenia Tsytrikova</h1>
+                        <h4>FRONTEND DEVELOPER</h4>
+                        <div className={s.socialBox}>
+                            <a href={'https://t.me/KseniaTst'}> <TelegramIcon fontSize={'large'} style={{
+                                color: 'white',
+                                paddingRight: '15px'
+                            }}/></a>
+                            <a href={'https://github.com/KseniaTst'}> <GitHubIcon fontSize={'large'} style={{
+                                color: 'white',
+                                paddingRight: '15px'
+                            }}/></a>
+                            <a href={'https://www.linkedin.com/in/ksenia-tsytrikova-993b66240/'}><LinkedInIcon
+                                fontSize={'large'} style={{color: 'white', paddingRight: '15px', height: '35px'}}/></a>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </MouseParallaxContainer>
         </div>
     )
 }
