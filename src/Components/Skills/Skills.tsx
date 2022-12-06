f0b055b3c8dc5738211ecaf7e242d6e700017ed2
Ksenia Tst <kseniatsytrikova@gmail.com>
2022-12-06 22:04:03 +0300

    change front page

--- src/Components/Skills/Skills.tsx
+++ src/Components/Skills/Skills.tsx
@@ -24,7 +24,7 @@ export const Skills = () => {
                     <motion.a download style={{color:'#dc965a', fontSize:'13px',
                         fontWeight:'300', letterSpacing:'4px', lineHeight:'1.5',
                     fontFamily:'Poppins, sans serif',textTransform:'uppercase' }}
-                       href={'https://drive.google.com/file/d/1giceClpAl1Z5WQ4pr1hhOjAh_voTbiL6/view?usp=sharing'}>
+                       href={'https://drive.google.com/file/d/1JaH33RELWzwfzmJ1s-e3ZCq0h3qlfA80/view?usp=sharing'}>
                         Download CV </motion.a>
                     </Fade>
                 </div>
